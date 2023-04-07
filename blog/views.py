a157d92b8721dd7abc68306783dc9ae982d43b17
yunpen06 <yunpen06@limefriends.com>
2023-04-07 09:55:27 +0900

    [feature] CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
+class PostList(ListView):
+    model = Post
+    # view 연결 1번 방법
+    # template_name = 'blog/index.html'
+    # 2번 방법 파일 이름을 ~~~_list.html
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# FBV
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
