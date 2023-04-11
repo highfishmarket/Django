62c71bf82d6437b88af93d8b7649832a7e9b7f57
yunpen06 <yunpen06@limefriends.com>
2023-04-11 09:24:30 +0900

    [feature] 포스트 목록 페이지에 대표 이미지 출력하기

--- prac_django_prj/settings.py
+++ prac_django_prj/settings.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 """
 Django settings for prac_django_prj project.
 
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
